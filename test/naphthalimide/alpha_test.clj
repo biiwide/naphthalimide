933610fa680b87f7062043b0400898157a5ca9d6
Ted Cushman <biiwide@users.noreply.github.com>
2018-04-12 11:55:44 -0500

    Committing WIP testing

--- test/naphthalimide/alpha_test.clj
+++ test/naphthalimide/alpha_test.clj
@@ -1,6 +1,9 @@
 (ns naphthalimide.alpha-test
   (:require [clojure.test :refer :all]
-            [naphthalimide.alpha :as trace]))
+            [naphthalimide.alpha :as trace]
+            [naphthalimide.alpha.span :as span]
+            [naphthalimide.alpha.tracer.mock :as mock]
+            ))
 
 
 
@@ -15,15 +18,38 @@
 
 
 (trace/defn traced-fn-1
-  [a b]
-  (+ a b))
+  ([a b]
+    (+ a b)))
+
 
 (trace/defn ^Number traced-fn-2
   "docstring"
-  ([] 1)
+  ([] 0)
   ([a] a)
-  ([a b] (* a b)))
+  ([a b]
+    (traced-fn-1 a b))
+  ([a b & more]
+    (reduce traced-fn-1 a (cons b more))))
+
 
 (trace/defn traced-fn-3
   [[a b] {:keys [c d]}]
   (list a b c d))
+
+
+(deftest test-nested-traced-fns
+  (let [fn1 (trace/fn fn1 [a b] (+ a b))
+        fn2 (trace/fn fn2
+              ([a b] (fn1 a b))
+              ([a b & more] (reduce fn1 a (cons b more))))]
+    #_(are [expected xform expr]
+          (let [tracer (mock/tracer)]
+            (try expr (catch Exception e nil))
+            (= expected
+               (map xform (mock/finished-spans tracer))))
+         
+          )))
+         
+         
+         
+         
